88c03c51fb758a478a4bacc7ad42bf9ad312688d
Benxia Hu <hubenxia@gmail.com>
2020-05-06 19:55:45 -0400

    Update and rename gimme.sh to 3-TF_motif.sh

--- NeuN_Loops/3-TF_motif.sh
+++ NeuN_Loops/3-TF_motif.sh
@@ -1,8 +1,5 @@
-###### super FIREs
-cd /proj/hyejunglab/epigenetics/NeuNsort/Panos/Loops_ATAC
 ## analyze differential motifs with gimme maelstrom
-echo -e "loc\tcluster"  > merged_peaks.txt
-
+cd /proj/hyejunglab/epigenetics/NeuNsort/Panos/Loops_ATAC
 peak=(glia_loops_ATACpeak.txt neuron_loops_ATACpeak.txt)
 sampleid=(NeuN- NeuN+)
 
@@ -15,11 +12,3 @@ conda create -n gimme python=3 gimmemotifs
 conda activate gimme
 genomepy install hg19 UCSC --annotation
 gimme maelstrom merged_peaks.txt hg19 merged_peaks_motif.out
-
-python
-from gimmemotifs.maelstrom import MaelstromResult
-import matplotlib.pyplot as plt
-mr = MaelstromResult("merged_peaks_motif.out/")
-fig = plt.figure(figsize=(10, 10))
-mr.plot_heatmap(threshold=3)
-plt.savefig("merged_peaks_motif.out/heatmap.pdf", bbox_inches = 'tight')
